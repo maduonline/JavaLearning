be390faa884d71cd66694639129e6f880f65705d
Praboda Ariyasinghe <me@maduonline.com>
2014-05-10 14:38:01 +0530

    Initial Commit.

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,24 @@
+package javaapplication1;
+
+/**
+ *
+ * @author Praboda
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  * *");
+        System.out.println(" * * *");
+        System.out.println("* * * *");
+        System.out.println();
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+
+}
